563ed1843bf562d689eab3e4ee5806a0e994340f
池ノ上将基 <ikenojoushoumoto@ikenojoushoumotonoMacBook-Air.local>
2025-06-01 23:48:21 +0900

    セクション５、６提出

--- php-practice_1.php
+++ php-practice_1.php
@@ -1,41 +1,143 @@
 <?php
 // Q1 変数と文字列
+$name = '安藤';
+$selfintro = '私の名前は「'. $name .'」です。';
+
+var_dump($selfintro);
 
 
 // Q2 四則演算
+$num = 1;
+$num = $num * 5 * 4;
+echo $num. "\n";
+$num/= 2;
+echo $num. "\n";
 
 
 // Q3 日付操作
+$datetime = new DateTime('2019-05-23 14:28:22');
+$date = '現在時刻は'.$datetime->format('Y年m月d日 H時i分s秒').'です。';
+echo $date;
 
 
 // Q4 条件分岐-1 if文
+$device = ['mac','windows','chrome','Unix'];
+$device = 'mac';
+if($device){
+    echo '使用OSは'. $device . 'です。';
+}else{
+    echo 'どちらでもありません。';
+}
 
 
 // Q5 条件分岐-2 三項演算子
+$age = 20;
+$message = ($age < 18)? '未成年です。': '成人です。';
+echo $message;
 
 
 // Q6 配列
+$prefecture = ['東京都','埼玉県','栃木県','千葉県','群馬県','茨城県','埼玉県'];
+$message = $prefecture[2].'と'. $prefecture[3].'は関東地方の都道府県です。';
+
+echo $message;
 
 
 // Q7 連想配列-1
+$prefectures = ['東京'=>'新宿区',
+                '神奈川県'=>'横浜市',
+                '千葉県'=>'千葉市',
+                '埼玉県'=>'さいたま市',
+                '栃木'=>'宇都宮市',
+                '群馬'=>'前橋市',
+                '茨城'=>'水戸'];
+foreach ($prefectures as $city) {
+    echo $city . "\n";
+}
 
 
 // Q8 連想配列-2
+$prefectures = ['東京都'=>'新宿区',
+                '神奈川県'=>'横浜市',
+                '千葉県'=>'千葉市',
+                '埼玉県'=>'さいたま市',
+                '栃木県'=>'宇都宮市',
+                '群馬県'=>'前橋市',
+                '茨城県'=>'水戸'];
+
+if (isset($prefectures['埼玉県'])) {
+    echo '埼玉県の県庁所在地は、' . $prefectures['埼玉県'] . 'です。';
+} else {
+    echo '埼玉県の県庁所在地は不明です。';
+}
 
 
 // Q9 連想配列-3
-
+$prefectures = ['東京都'=>'新宿区',
+                '神奈川県'=>'横浜市',
+                '千葉県'=>'千葉市',
+                '埼玉県'=>'さいたま市',
+                '栃木県'=>'宇都宮市',
+                '群馬県'=>'前橋市',
+                '茨城県'=>'水戸',
+                '宮崎県'=>'宮崎',
+                '鹿児島県'=>'鹿児島'];
+                
+if (isset($prefectures['東京','神奈川県','千葉県','埼玉県',]))
 
 // Q10 関数-1
+function hello($name){
+    echo $name . 'さん、こんにちは。'."\n";
+}
+
+hello('金谷');
+hello('安藤');
 
 
 // Q11 関数-2
+function calcTaxInPrice($price){
+    $taxInPrice = $price * 1.1;
+    echo $price . 'の商品の税込価格は' . $taxInPrice . 'です。';
+}
+
+calcTaxInPrice(1000);
 
 
 // Q12 関数とif文
+function distinguishNum($number) {
+    if($number % 2 === 1){
+        echo $number . 'は奇数です。'."\n";
+    }else{
+        echo $number . 'は偶数です。'."\n";
+    }
+}
+
+distinguishNum(11);
+distinguishNum(24);
 
 
 // Q13 関数とswitch文
+function evaluateGrade($score){
+    switch($score){
+        case 'A':
+            echo '合格です。'. "\n";
+            break;
+        case 'B':
+            echo '合格です。'."\n";
+            break;
+        case 'C':
+            echo '合格ですが追加課題があります。'."\n";
+            break;
+        case 'D':
+            echo '不合格です。'."\n";
+            break;
+        default:
+            echo'判定不明です。講師に問い合わせてください。'."\n";
+    }
+}
+
+evaluateGrade('B');
+evaluateGrade('G');
 
 
 ?>
--- php-practice_2.php
+++ php-practice_2.php
@@ -1,15 +1,92 @@
 <?php
 // Q1 tic-tac問題
 
+function tictac($i){
+    if($i % 4 === 0 && $i % 5 === 0){
+        echo 'tic-tac'."\n";
+    }elseif($i % 5 === 0){
+        echo 'tac'."\n";
+    }elseif($i % 4 === 0){
+        echo 'tic'."\n";
+    }else{
+        echo $i."\n";
+    }
+}
+for ($i=1; $i<=100; $i++){
+tictac($i);
+}
 
 // Q2 多次元連想配列
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+];
+
+//問題１
+echo ($personalInfos[1]['name']).'の電話番号は'.($personalInfos[1]['tel']).'です。';
+
+
+//問題２
+$index = 1;
+foreach ($personalInfos as $info) {
+    echo "{$index}番目の{$info['name']}のメールアドレスは{$info['mail']}で、電話番号は{$info['tel']}です。" . "\n";
+    $index++;
+}
+
+//問題３
+$ageList = [25, 30, 18];
+foreach ($personalInfos as $i => &$info) {
+    $info['age'] = $ageList[$i];
+}
+var_dump($personalInfos);
 
 
 // Q3 オブジェクト-1
+class Student
+{
+    public $studentId;
+    public $studentName;
+
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend()
+    {
+        echo '授業に出席しました。';
+    }
+}
+
+$Yamada = new Student('120','山田');
+echo ('学籍番号' . $Yamada->studentId . '番の生徒は'. $Yamada->studentName . 'です。');
 
 
 // Q4 オブジェクト-2
+$date = new Datetime('2021-03-02');
+$date -> modify('-1 month');
+echo $date-> format('Y-m-d');
 
 
 // Q5 定義済みクラス
+$date1 = strtotime('2021-03-02');
+$date2 = strtotime('1992-04-25');
+
+$date3 = ($date1 - $date2)/ 86400;
+$datetime = 'あの日から' . $date3 . '日経過しました。';
+echo $datetime;
 ?>
